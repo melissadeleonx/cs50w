4c1da4b3f47da7b291928d5c49dd8f060febe531
melissadeleonx <primavita19@gmail.com>
2024-02-24 15:47:20 +0100

    leaning basic of Python

---
+++ name.py
@@ -0,0 +1,17 @@
+# name = input("Name: ")
+# print("Hello, " + name)
+
+# name = "Melissa"
+# age = 25
+# print(f"My name is {name} and I am {age} years oldr")
+
+# num1 = 10
+# num2 = 5
+# print(f"The sum of {num1} and {num2} is {num1 + num2}.")
+
+# def greet(name):
+#     return f"Hello, {name}!"
+# print(greet("Alice"))
+
+pi = 3.14159
+print(f"The value of pi is {pi:.2f}.")
