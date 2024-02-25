b60664c2f01cb8aba3154faa3d195c69a5dacb96
melissadeleonx <primavita19@gmail.com>
2024-02-25 15:33:51 +0100

    change the filename to list.py

---
+++ list.py
@@ -0,0 +1,48 @@
+# Creating a list - List consists of mutable sequences
+my_list = [1, 2, 3, 'a', 'b', 'c']
+
+# Indexing: Accessing elements by index
+# print("Element at index 0:", my_list[0]) # Output 1
+# print("Element at index 3:", my_list[3]) # Output 2
+
+# Slicing: Getting a sublist
+# print("Sublist from index 2 to 4:", my_list[2:5]) 
+
+# Appending: Adding elements to the end of the list
+# my_list.append('d')
+# print("After appending 'd':", my_list)
+
+# my_list.append('e')
+# print("Testing the new list:", my_list)
+
+# Inserting: The insert() method takes two arguments: the index at which to insert the element and the element itself. 
+# my_list.insert(3, 4)
+# print(my_list)
+
+# my_list.insert(4, 'z')
+# print("The new list:", my_list)
+
+# Removing: removing element by value
+# my_list.remove(3)
+# print(my_list)
+
+# Deleting by index, we can use the method del or pop()
+# del my_list[3]
+# print(my_list)
+
+# my_list.pop(4)
+# print(my_list)
+
+# Length: Getting the length of the list using len
+# print("Length of the list:", len(my_list))
+
+# Iterating: Looping through elements of the list
+# print("Elements of the list:")
+# for item in my_list:
+#     print(item)
+
+# Checking membership or existence in the list
+# print("Is 'b' in the list?", 'b' in my_list)
+
+# print('z' in my_list)
+
